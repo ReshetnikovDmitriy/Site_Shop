5bf09fdfcb8ba5a33ab31927eb5d4bc9d2164f48
Reshetnikov Dmitriy <Reshetnikov.111@mail.ru>
2025-03-28 18:45:37 +0300

    добавил блоки main и offer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,41 @@
+import 'normalize.css';
 import './index.html';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1
+        },
+        768: {
+            slidesPerView: 2,
+            slidesPerGroup: 2
+        },
+        1000: {
+            slidesPerView: 3,
+            slidesPerGroup: 3
+        }
+    }
+});
